1278058688db030bc5a2c8d2e516083f06cd2c07
rthomson412 <96792780+rthomson412@users.noreply.github.com>
2022-01-06 10:46:25 -0500

    Create population.c

---
+++ population.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    
+    int start;
+        do
+        {
+        start = get_int("starting population: ");
+    }
+    while(start < 9);
+
+    // TODO: Prompt for end size
+    
+    int end;
+    do
+    {
+       end = get_int("ending population: ");
+    }
+    while(start > end);
+    
+
+    // TODO: Calculate number of years until we reach threshold
+    
+    int year = 0;
+    do
+    {
+        start = start + (start/3) - (start/4);
+        year++;
+    }
+    while(start < end);
+    
+
+    // TODO: Print number of years
+    
+    printf("Years: %i", year);
+}
